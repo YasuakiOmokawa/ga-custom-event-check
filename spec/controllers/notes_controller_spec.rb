bcf509cb5967fc16b62b7269c76f06fc07b441ba
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2019-07-14 03:17:40 +0000

    comment additional message

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe NotesController, type: :controller do
 
   let(:user) { double("user") }
+  # 後続の記述でownerとid属性を使うので、instance_doubleを使ってバリデーションしている
   let(:project) { instance_double("Project", owner: user, id: "123") }
 
+  # パスワード認証が求められる箇所と、データベースアクセスが実施される箇所をテストダブルを
+  # 返却するように変更
   before do
     allow(request.env["warden"]).to receive(:authenticate!).and_return(user)
     allow(controller).to receive(:current_user).and_return(user)
@@ -13,8 +16,12 @@ RSpec.describe NotesController, type: :controller do
 
   describe "#index" do
     it "searches notes by the provided keyword" do
+      # projectに関連するnotesが持つsearchスコープが呼ばれることと、
+      # その際の検索キーワードであるtermが同名のパラメータ値に一致することを検証。
+      # エクスペクテーションはアプリコードを動かす前に書かないといけない。
       expect(project).to receive_message_chain(:notes, :search).
         with("rotate tires")
+
       get :index,
         params: { project_id: project.id, term: "rotate tires" }
     end
